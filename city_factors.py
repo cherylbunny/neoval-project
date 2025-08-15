836d90056d73d700ff8ca6ec574752cfc35d0efc
Will Sijp <wsijp@raywhite.com>
2025-08-15 15:13:58 +1000

    Now plotting all three PC components and comparing to factor trends, renamed figure.

--- city_factors.py
+++ city_factors.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import os
+import argparse
 
 home_dir = os.path.expanduser("~")
 paper_path = os.path.join(home_dir, 'Documents/github/neoval-project')
@@ -12,11 +13,16 @@ data_path = os.path.join(paper_path, 'data')
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--factor', help = "Factor to plot.", type=int, default = 1, required=False)
+
+    show_factor = parser.parse_args().factor - 1
+
     df = pd.read_csv(os.path.join(data_path, 'df_factor_trends.csv'))
     df_pcs = pd.read_csv(os.path.join(data_path, 'df_pcs.csv'))
     df_p = df_pcs.pivot(columns='mode', index='period', values='pc_value')
 
-
+    factors = ["market", "mining", "lifestyle"]
 
     df = df.set_index('month_date')
     df.index = pd.to_datetime(df.index)
@@ -25,16 +31,16 @@ if __name__ == '__main__':
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
     # Left y-axis: Brisbane - Sydney spread
-    series1 = df['mining']
-    ax1.plot(series1.index, series1, color='tab:blue', label='Greater Brisbane − Greater Sydney')
-    ax1.set_ylabel('House Price Index Difference\n(Brisbane − Sydney, % growth)', color='tab:blue')
+    series1 = df[factors[show_factor]]
+    ax1.plot(series1.index, series1, color='tab:blue', label=f'Factor {show_factor+1}')
+    ax1.set_ylabel(f'Factor {show_factor+1}', color='tab:blue')
     ax1.tick_params(axis='y', labelcolor='tab:blue')
 
     # Right y-axis: PC1 from df_p
-    series2 = df_p[1]
+    series2 = df_p[show_factor]
     ax2 = ax1.twinx()
-    ax2.plot(series2.index, series2, color='tab:red', label='PC1: [Insert interpretation]')
-    ax2.set_ylabel('Principal Component 1 (standardized units)', color='tab:red')
+    ax2.plot(series2.index, series2, color='tab:red', label=f'PC{show_factor+1}')
+    ax2.set_ylabel(f'Principal Component {show_factor+1} (standardized units)', color='tab:red')
     ax2.tick_params(axis='y', labelcolor='tab:red')
 
     # Title and legends
@@ -46,5 +52,5 @@ if __name__ == '__main__':
     ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper left')
 
     fig.tight_layout()
-    plt.savefig(os.path.join(paper_path, 'figures', 'brisbane_sydney_pc1_comparison.pdf'))
+    plt.savefig(os.path.join(paper_path, 'figures', f'factor{show_factor+1}.pdf'))
 
